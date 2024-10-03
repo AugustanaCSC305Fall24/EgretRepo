bba828707d7113020d4a223ebb0a52ecd0723887
Landon Wierenga <landonwierenga23@augustana.edu>
2024-10-03 11:22:55 -0500

    Added method descriptions

--- HamSimProj/src/main/java/edu/augustana/CWHandler.java
+++ HamSimProj/src/main/java/edu/augustana/CWHandler.java
@@ -7,16 +7,27 @@ public class CWHandler {
     private static ArrayList<String> cwArray = new ArrayList<String>();
     private static long startTime;
 
+
+    /**
+     * creates a starting point for the timer
+     */
     public static void startTimer() {
         startTime = System.nanoTime();
 
     }
 
+    /**
+     * takes the starting point of the timer and subtracts it from the current time to get the time pressed
+     */
     public static void stopTimer() {
         long timePressed = System.nanoTime() - startTime;
         convertToCW(timePressed);
     }
 
+    /**
+     * converts the time pressed to a dot or a dash
+     * @param timePressed the time that the button was pressed for
+     */
     public static void convertToCW(long timePressed) {
         String cwCharacter = "";
         if (timePressed < 200000000) {
