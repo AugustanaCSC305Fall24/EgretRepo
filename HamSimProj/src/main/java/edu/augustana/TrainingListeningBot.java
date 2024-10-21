2e4a5ce18e3ed2dc6229697033bc14ab5e63c28b
Landon Wierenga <landonwierenga23@augustana.edu>
2024-10-20 23:07:37 -0500

    merged the branches and then commented out some of the code in the listening bot class because it wasn't working

--- HamSimProj/src/main/java/edu/augustana/TrainingListeningBot.java
+++ HamSimProj/src/main/java/edu/augustana/TrainingListeningBot.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class TrainingListeningBot {
 
     private double outputFrequency;
-    private final String botPhrase;
-    private final String callSign;
+    //private final String botPhrase;
+    //private final String callSign;
     private final int band;
 
 
@@ -17,11 +17,11 @@ public class TrainingListeningBot {
 
     public TrainingListeningBot(int band) {
         this.band = 10; //temporary. Setting the band to 10
-        this.botPhrase = botPhraseArray[randomGen.nextInt(0, botPhraseArray.length)];
-        this.callSign = botCallSignArray[randomGen.nextInt(0, botCallSignArray.length)];
+        //this.botPhrase = botPhraseArray[randomGen.nextInt(0, botPhraseArray.length)];
+        //this.callSign = botCallSignArray[randomGen.nextInt(0, botCallSignArray.length)];
 
         if (band == 10) { //need to add more if else statements to account for each band option
-            this.outputFrequency = randomGen.nextDouble(28.000, 29.700);
+            //this.outputFrequency = randomGen.nextDouble(28.000, 29.700);
         }
 
     }
@@ -30,17 +30,17 @@ public class TrainingListeningBot {
      * Accessor method for botPhrase
      * @return botPhrase
      */
-    public String getBotPhrase() {
-        return botPhrase;
-    }
+//    public String getBotPhrase() {
+//        return botPhrase;
+//    }
 
     /**
      * Accessor method for callSign
      * @return callSign
      */
-    public String getCallSign() {
-        return callSign;
-    }
+//    public String getCallSign() {
+//        return callSign;
+//    }
 
     /**
      * Accessor method for outputFrequency
