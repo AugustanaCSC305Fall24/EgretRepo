bcb35cecf95337cc5617cc2b955915cd2b927068
Landon Wierenga <landonwierenga23@augustana.edu>
2024-10-31 23:00:18 -0500

    Just made the training listening bots play their sounds in their own threads so that the app doesn't crash. The training is almost complete, but the check guess doesn't work right.

--- HamSimProj/src/main/java/edu/augustana/TrainingListeningBot.java
+++ HamSimProj/src/main/java/edu/augustana/TrainingListeningBot.java
@@ -12,14 +12,15 @@ public class TrainingListeningBot {
 
 
     public static final String[] botPhraseArray = {"Hello", "Hello2"}; //Add the phrases to this list
-    public static final String[] botCallSignArray = {"HHTO", "PTOY"}; //Add list of call signs
+    public static final String[] botCallSignArray = {"1", "2"}; //Add list of call signs
     private static final Random randomGen = new Random();
 
 
     public TrainingListeningBot(int band) throws InterruptedException {
         this.band = 10; //temporary. Setting the band to 10
-        this.botPhrase = TextToMorseConverter.textToMorse(botPhraseArray[randomGen.nextInt(botPhraseArray.length)]); //Morse string of their phrase
-        this.callSign = TextToMorseConverter.textToMorse(botCallSignArray[randomGen.nextInt(botCallSignArray.length)]); //Morse string of their callSign
+        //will have to remove the selection from the list so that two bots don't have the same stuff
+        this.botPhrase = TextToMorseConverter.textToMorse(botPhraseArray[randomGen.nextInt(botPhraseArray.length) - 1]); //Morse string of their phrase
+        this.callSign = TextToMorseConverter.textToMorse(botCallSignArray[randomGen.nextInt(botCallSignArray.length) - 1]); //Morse string of their callSign
         this.playSound = false;
 
         if (band == 10) { //need to add more if else statements to account for each band option
@@ -63,15 +64,35 @@ public class TrainingListeningBot {
         playSound = false;
     }
 
-    private void playContinuousMessage() throws InterruptedException {
+    private void playContinuousMessage() {
 
         //Can probably have this just play the whole message, but when I do I need to add more space between the call sign
         //and the message. So just append an * between the two I think.
-        while (playSound) {
-            MorsePlayer.playBotMorseString(callSign, outputFrequency);
-            Thread.sleep(100000000); //Need to adjust this to wait the right amount of time
-            MorsePlayer.playBotMorseString(botPhrase, outputFrequency);
-            Thread.sleep(1000000000); //Need to adjust this too. Needs to be long enough to wait for the entire message to play before starting the while loop again
-        }
+
+        new Thread(() -> {
+            while (playSound) {
+                try {
+                    MorsePlayer.playBotMorseString(callSign, outputFrequency);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                try {
+                    Thread.sleep(4000); //Need to adjust this to wait the right amount of time. Needs to change to match the beat length etc.
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                try {
+                    MorsePlayer.playBotMorseString(botPhrase, outputFrequency);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                try {
+                    Thread.sleep(4000); //Need to adjust this too. Needs to be long enough to wait for the entire message to play before starting the while loop again
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+
     }
 }
