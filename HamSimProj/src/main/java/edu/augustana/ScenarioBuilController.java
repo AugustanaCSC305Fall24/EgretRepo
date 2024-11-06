e7ea7cf38931ae96085a48b454ccbfdfe31eb7d7
Kevin Rodriguez <rodriguezkn60@gmail.com>
2024-11-06 10:01:04 -0600

    Added functionality in the controllers of scenario builder

--- HamSimProj/src/main/java/edu/augustana/ScenarioBuilController.java
+++ HamSimProj/src/main/java/edu/augustana/ScenarioBuilController.java
@@ -1,11 +1,77 @@
 package edu.augustana;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ScenarioBuilController {
 
     @FXML
-    void initialize(
+    private Button addBotBtn;
 
-    ){}
+    @FXML
+    private TableView<?> botListTable;
+
+    @FXML
+    private Button cancelBtn;
+
+    @FXML
+    private Button deletebotBtn;
+
+    @FXML
+    private TextArea descripTionField;
+
+    @FXML
+    private TextField humidityField;
+
+    @FXML
+    private Button saveScenarioBtn;
+
+    @FXML
+    private TextField scenarioNameField;
+
+    @FXML
+    private ChoiceBox<?> scenarioTypeChoice;
+
+    @FXML
+    private TextField solarIndex;
+
+    @FXML
+    private TextField tempField;
+
+    @FXML
+    private TextField timeField;
+
+    @FXML
+    private TextArea userMessageField;
+
+    @FXML
+    private TextField windSpeedField;
+
+
+    @FXML
+    void initialize(){
+
+        addBotBtn.setOnAction(event -> {
+            try {
+                openBotAdder();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+    }
+
+
+    private void openBotAdder() throws IOException {
+        Stage scenarioBuildStage = new Stage();
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("botAdder.fxml"));
+        scenarioBuildStage.setTitle("Scenario Builder");
+        scenarioBuildStage.setScene(new Scene(loader.load()));
+        scenarioBuildStage.show();
+    }
 }
