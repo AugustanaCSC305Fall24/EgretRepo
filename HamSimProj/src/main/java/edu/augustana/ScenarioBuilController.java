b6127b96eb32868061f6b075a722c4fb0a7acdaa
Kevin Rodriguez <rodriguezkn60@gmail.com>
2024-11-06 18:16:21 -0600

    Worked on the scenario builder and added saving scenario and displaying variables

--- HamSimProj/src/main/java/edu/augustana/ScenarioBuilController.java
+++ HamSimProj/src/main/java/edu/augustana/ScenarioBuilController.java
@@ -1,12 +1,16 @@
 package edu.augustana;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class ScenarioBuilController {
 
@@ -35,7 +39,7 @@ public class ScenarioBuilController {
     private TextField scenarioNameField;
 
     @FXML
-    private ChoiceBox<?> scenarioTypeChoice;
+    private ChoiceBox<Integer> scenarioTypeChoice;
 
     @FXML
     private TextField solarIndex;
@@ -52,9 +56,23 @@ public class ScenarioBuilController {
     @FXML
     private TextField windSpeedField;
 
+    private SimScenario scenario;
+
+    private botAdderController adderController;
+
+    private BotCollection botCollection;
+
+    private RadioEnviroment environment;
+
+    private SandboxController parentController;
+
+    private boolean isNewScenario = true;
+
 
     @FXML
     void initialize(){
+
+        scenarioTypeChoice.setValue(0);
 
         addBotBtn.setOnAction(event -> {
             try {
@@ -64,14 +82,87 @@ public class ScenarioBuilController {
             }
         });
 
+        cancelBtn.setOnAction(evt -> Platform.exit());
+
+        saveScenarioBtn.setOnAction(event -> {
+
+            createScenario();
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.close();
+
+        });
+
+    }
+
+    public void setScenario(SimScenario scenario){
+        this.scenario = scenario;
     }
 
 
     private void openBotAdder() throws IOException {
         Stage scenarioBuildStage = new Stage();
         FXMLLoader loader = new FXMLLoader(App.class.getResource("botAdder.fxml"));
+
         scenarioBuildStage.setTitle("Scenario Builder");
         scenarioBuildStage.setScene(new Scene(loader.load()));
         scenarioBuildStage.show();
+
+        adderController = loader.getController();
+        adderController.setBotCollection(botCollection);
     }
+
+    public void editScenario(SimScenario scenario){
+
+        isNewScenario = false;
+
+        this.scenario = scenario;
+        environment  = scenario.getEnvironment();
+        botCollection  = scenario.getBotCollection();
+
+        scenarioNameField.setText(scenario.getName());
+        userMessageField.setText(scenario.getUserMessage());
+        timeField.setText("00:00");
+        tempField.setText(String.valueOf(environment.temperature));
+        humidityField.setText(String.valueOf(String.valueOf(environment.humidity)));
+        windSpeedField.setText(String.valueOf(environment.windSpeed));
+        solarIndex.setText(String.valueOf(environment.solarActivity));
+        scenarioTypeChoice.setValue(scenario.getType());
+
+
+
+
+    }
+
+    public void newScenario(){
+        botCollection = new BotCollection(new ArrayList<TrainingListeningBot>());
+    }
+
+    private void createScenario(){
+
+        if(isNewScenario){
+
+            environment = new RadioEnviroment("scenarioNameField.getText()",
+                    Double.parseDouble(solarIndex.getText()),
+                    Double.parseDouble(windSpeedField.getText()),
+                    Double.parseDouble(humidityField.getText()),
+                    Double.parseDouble(tempField.getText()));
+            SimScenario newScenario = new SimScenario(scenarioNameField.getText(),descripTionField.getText(),userMessageField.getText(),"That is not right!", "You Win!",
+                    environment, botCollection, scenarioTypeChoice.getValue());
+
+            ScenarioCollection.addScenario(newScenario);
+            parentController.updateScenarioChoice();
+        }else{
+
+        }
+
+
+    }
+
+
+    public void setParentController(SandboxController controller){
+        parentController = controller;
+    }
+
+
 }
--- HamSimProj/src/main/java/edu/augustana/botAdderController.java
+++ HamSimProj/src/main/java/edu/augustana/botAdderController.java
@@ -1,4 +1,73 @@
 package edu.augustana;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class botAdderController {
+
+    @FXML
+    private Button addBtn;
+
+    @FXML
+    private TextField botNameField;
+
+    @FXML
+    private TextField callSignField;
+
+    @FXML
+    private Button cancelBtn;
+
+    @FXML
+    private TextField messageField;
+
+    private SimScenario scenario;
+
+    private BotCollection botCollection;
+
+    @FXML
+    private Button okBtn;
+
+    @FXML
+    void initialize(){
+
+        addBtn.setOnAction( event -> {
+            try {
+                addBot();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        });
+
+        okBtn.setOnAction(evt -> {
+            Stage stage = (Stage) ((Node) evt.getSource()).getScene().getWindow();
+            stage.close();
+        });
+
+        cancelBtn.setOnAction(evt -> {
+            Stage stage = (Stage) ((Node) evt.getSource()).getScene().getWindow();
+            stage.close();
+        });
+
+    }
+
+
+    public void setBotCollection(BotCollection collection){
+        botCollection = collection;
+    }
+
+    private void addBot() throws InterruptedException {
+        TrainingListeningBot bot = new TrainingListeningBot(Radio.getBand());
+
+        botCollection.addBot(bot);
+
+
+
+    }
+
+
 }
